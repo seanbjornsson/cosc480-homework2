4949655cc5ce2f27d175b6424000a0fc17ff0545
sbjornsson <sbjornsson@colgate.edu>
2013-02-20 12:37:29 -0500

    stage 2 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,34 @@
 # in app/controllers/movies_controller.rb
 
 class MoviesController < ApplicationController
-  def index
+  def arr_to_hash array
+		hash = Hash.new()
+		array.each do |elt|
+			hash.store(elt,1)
+		end
+		hash
+	end
+
+	def index
+		debugger
+		@sort = params[:param]
 		@all_ratings = Movie.ratings
+		@all_ratings_hash = arr_to_hash(@all_ratings)
+
 		if @checked_ratings == nil
 			@checked_ratings = @all_ratings
+		end	
+		@checked_ratings_hash = params[:ratings] unless params[:ratings] == nil
+		@checked_ratings = @checked_ratings_hash.keys unless @checked_ratings_hash == nil
+
+		@movies = Movie.find_all_by_rating(@checked_ratings,:order => @sort)
+		@title_hilite = ("hilite" if @sort=="title")
+		@release_date_hilite = ("hilite" if @sort=="release_date")
+
+		if params[:ratings]==nil
+			@checked_ratings_hash = arr_to_hash(@checked_ratings)
+			redirect_to :action => 'index', :ratings => @checked_ratings_hash, :param => @sort
 		end
-		@checked_ratings = params[:ratings].keys unless params[:ratings] == nil
-		@movies = Movie.find_all_by_rating(@checked_ratings,:order => params[:param])
-		@title_hilite = ("hilite" if params[:param]=="title")
-		@release_date_hilite = ("hilite" if params[:param]=="release_date")
-		
   end
 
   def show
